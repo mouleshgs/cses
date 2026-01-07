b83093c69cd9350da05ada30f117d4f5ed621d41
moulesh <mouleshleo06@gmail.com>
2026-01-07 23:53:12 +0530

    towers and playlist problems added

---
+++ Playlist.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.*;
+
+public class Playlist {
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = fs.nextInt();
+
+        long[] arr = new long[n];
+        for (int i = 0;i < n;i++) {
+           arr[i] = fs.nextLong();
+        }
+        
+        int left = 0;
+
+        int ans = 0;
+
+        Set<Long> set = new HashSet<>();
+        for (int right = 0;right < n;right++) {
+
+            
+            while (left < n && set.contains(arr[right])) {
+                
+                set.remove(arr[left]);
+                left++;
+            }
+            
+            set.add(arr[right]);
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        out.println(ans);
+
+        out.flush();
+    }
+
+    static class FastScanner {
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final InputStream in = System.in;
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c, sign = 1;
+            long val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            StringBuilder sb = new StringBuilder();
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        String nextLine() throws IOException {
+           int c;
+           StringBuilder sb = new StringBuilder();
+           while (true) {
+               c = readByte();
+               if (c == -1) return sb.toString();
+               if (c != '\n' && c != '\r') break;
+           }
+
+           while (c != -1 && c != '\n' && c != '\r') {
+               sb.append((char) c);
+               c = readByte();
+           }
+           return sb.toString();
+        }
+    }
+}
