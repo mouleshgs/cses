ceaed766f697d5781dbff4453e1d2bd7a375e529
moulesh <mouleshleo06@gmail.com>
2026-01-19 23:50:54 +0530

    changed

---
+++ FactoryMachines.java
@@ -0,0 +1,133 @@
+import java.io.*;
+import java.util.*;
+
+public class FactoryMachines {
+
+    public static boolean check(long mid, long[] arr, long t) {
+
+        long total = 0L;
+        for (long x : arr) {
+            total += mid / x;
+        }
+
+        return total >= t;
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = fs.nextInt();
+
+        long t = fs.nextLong();
+
+        long[] arr = new long[n];
+        long min = Long.MAX_VALUE;
+        for (int i = 0;i < n;i++) {
+           arr[i] = fs.nextLong();
+           min = Math.min(arr[i], min);
+        }
+
+
+
+        long l = 0;
+        long r = min * t;
+        long ans = -1;
+
+        while (l <= r) {
+            long mid = l + (r - l) / 2;
+            if (check(mid, arr, t)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+
+        out.println(ans);
+        out.flush();
+    }
+
+    static class FastScanner {
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final InputStream in = System.in;
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c, sign = 1;
+            long val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            StringBuilder sb = new StringBuilder();
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        String nextLine() throws IOException {
+           int c;
+           StringBuilder sb = new StringBuilder();
+           while (true) {
+               c = readByte();
+               if (c == -1) return sb.toString();
+               if (c != '\n' && c != '\r') break;
+           }
+
+           while (c != -1 && c != '\n' && c != '\r') {
+               sb.append((char) c);
+               c = readByte();
+           }
+           return sb.toString();
+        }
+    }
+}
---
+++ RoomAllocation.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.util.*;
+
+public class RoomAllocation {
+
+    static class Pair {
+        int a, b, i;
+
+        Pair(int a, int b, int i) {
+            this.a = a;
+            this.b = b;
+            this.i = i;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = fs.nextInt();
+
+        Pair[] arr = new Pair[n];
+        for (int i = 0;i < n;i++) {
+            int a = fs.nextInt();
+            int b = fs.nextInt(); 
+            arr[i] = new Pair(a, b, i);
+        }
+
+        Arrays.sort(arr, (x, y) -> Integer.compare(x.a, y.a));  
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> Integer.compare(x[0], y[0]));
+
+        int[] ans = new int[n];
+        int room = 0;
+
+        for (Pair c : arr) {
+
+            if (!pq.isEmpty() && pq.peek()[0] < c.a) {
+                int[] top = pq.poll();
+                int r = top[1];
+
+                ans[c.i] = r;
+                pq.offer(new int[]{c.b, r});
+            } else {
+                room++;
+                ans[c.i] = room;
+                pq.offer(new int[]{c.b, room});
+            }
+        }
+
+        out.println(room);
+
+        for (int val : ans) {
+            out.print(val + " ");
+        }
+
+        out.println();
+        
+        out.flush();
+    }
+
+    static class FastScanner {
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final InputStream in = System.in;
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c, sign = 1;
+            long val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            StringBuilder sb = new StringBuilder();
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        String nextLine() throws IOException {
+           int c;
+           StringBuilder sb = new StringBuilder();
+           while (true) {
+               c = readByte();
+               if (c == -1) return sb.toString();
+               if (c != '\n' && c != '\r') break;
+           }
+
+           while (c != -1 && c != '\n' && c != '\r') {
+               sb.append((char) c);
+               c = readByte();
+           }
+           return sb.toString();
+        }
+    }
+}
